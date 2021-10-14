52b42aa630329e5edaa73489a4a0accbaa7235f5
dejjv <dejjv@LAPTOP-EUR2LE7S>
2021-10-14 21:35:49 +0200

    Add project files.

---
+++ Reklamacka/Reklamacka/Pages/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Reklamacka.Pages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Reklamacka
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Test presunuti se na dalsi stranku
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private async void Button_Clicked(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new ItemEditPage());
+		}
+	}
+}
