7c46959d47ce7621b4912ff9a0a69260f2983244
David Kedra <dejjvit@gmail.com>
2021-12-03 21:32:17 +0100

    New Design

--- Reklamacka/Reklamacka/Converters/ExpColorConverter.cs
+++ Reklamacka/Reklamacka/Converters/ExpColorConverter.cs
@@ -20,12 +20,12 @@ namespace Reklamacka.Converters
 				if (DateTime.Compare((DateTime)value, DateTime.Today) < 0)                      // uz vyprsela
 					return Color.DarkGray;
 				else if (DateTime.Compare((DateTime)value, DateTime.Today.AddDays(21)) < 0)     // 21 dnu
-					return Color.FromHex("#ddff0000");
+					return Color.FromHex("#e77674");
 				else if (DateTime.Compare((DateTime)value, DateTime.Today.AddMonths(2)) < 0)    // 2 mesice
-					return Color.FromHex("#ddffa500");
+					return Color.FromHex("#eaab81");
 				else if (DateTime.Compare((DateTime)value, DateTime.Today.AddMonths(3)) < 0)    // 3 mesice
-					return Color.FromHex("#ddffff00");
-				return Color.FromHex("#dd00ff00");
+					return Color.FromHex("#f1e06b");
+				return Color.FromHex("#61d384");
 			}
 			catch
 			{
